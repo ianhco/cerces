32cc32e626cd03c7237a5969378cf381ae4baad4
Ian Huang <58193352+iann838@users.noreply.github.com>
2026-03-27 19:24:35 -0400

    chore: bump version 2.0.2 for aws-lambda fix

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -52,7 +52,7 @@ export default {
                 activeMatch: "/reference/*"
             },
             {
-                text: "2.0.1",
+                text: "2.0.2",
                 items: [
                     {
                         text: "Migrations",
